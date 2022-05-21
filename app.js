dd1fa44145a9cee833ef654ffbb6e6a34b673970
Holman Morales <hmoralesguido@gmail.com>
2022-05-21 11:07:40 -0600

    mejora

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 
   AOS.init();
+
+
+
+  const navlink = document.querySelectorAll(".nav__link");
+
+function activeLink() {
+  navlink.forEach((item) => item.classList.remove("active-link"));
+  this.classList.add("active-link");
+}
+
+navlink.forEach((item) => item.addEventListener("mouseover", activeLink));
